38577e163a0c4b91c4096bfd9006a4f23726542b
droide13 <roiserna@proton.me>
2026-09-16 15:58:06 +0200

    [ FIX ] Broken dependencies

--- src/tabs/__init__.py
+++ src/tabs/__init__.py
@@ -2,7 +2,7 @@
 
 from .cookies import CookiesTab
 from .dissemination.dissemination_ui import DisseminationTab
-from .identifiers.identifiers import IdentifiersTab
+from .identifiers.identifiers_ui import IdentifiersTab
 from .metadata.metadata_ui import MetadataTab
 from .networklog import NetworkLogTab
 from .overview.overview_ui import OverviewTab
